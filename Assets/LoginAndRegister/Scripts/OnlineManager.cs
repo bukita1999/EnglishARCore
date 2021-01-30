c61c6b079af952fa29a972f35e5c41b0d2441650
bukita1999 <47375498+bukita1999@users.noreply.github.com>
2021-01-30 13:08:25 +0800

    完成了Nuget的包导入以及Mysql.data动态链接库

---
+++ Assets/LoginAndRegister/Scripts/OnlineManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MySql.Data.MySqlClient;
+
+public class OnlineManager : MonoBehaviour
+{
+    
+    public InputField signup_user_inputfield;
+    public InputField signup_password_inputfield;
+    public static MySqlConnection mySqlConnection;
+    private static string database = "test";
+    private static string host = "127.0.0.1";
+    private static string id = "root";
+    private static string pwd = "123";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Register()
+    {
+
+    }
+}
